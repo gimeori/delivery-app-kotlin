9f0109e61bb3464f68e34ec5a94b78bb1737d915
gimeori <elizavetka07@gmail.com>
2024-10-23 13:48:35 +0700

    add yandexmap geo settings

--- app/src/main/java/com/example/deliveryapp/OrderActivity.kt
+++ app/src/main/java/com/example/deliveryapp/OrderActivity.kt
@@ -1,6 +1,9 @@
 package com.example.deliveryapp
 
 import android.os.Bundle
+import android.view.View
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,12 +20,27 @@ class OrderActivity : AppCompatActivity() {
         val orderId=intent.getIntExtra("orderId",0)
         val orderTotalPrice=intent.getStringExtra("orderTotalPrice")
         val orderStatus=intent.getStringExtra("orderStatus")
+        val webView = findViewById<WebView>(R.id.yandexMap)
+
         binding.orderDetailsId.text=orderId.toString()
         binding.orderDetailsStatus.text=orderStatus
         binding.orderDetailsTotalPrice.text=orderTotalPrice
         binding.backActiveOrders.setOnClickListener{
             finish()
         }
+        binding.takeOrder.setOnClickListener{
+            webView.settings.javaScriptEnabled = true
+            webView.settings.domStorageEnabled = true
+            webView.settings.javaScriptCanOpenWindowsAutomatically = true
+            webView.settings.setGeolocationEnabled(true)
+            webView.webViewClient = WebViewClient()
+
+            webView.loadUrl("file:///android_asset/geolocated_multiroute.html")
+            binding.orderDetailsStatus.text="отправлен"
+            binding.takeOrder.text="завершить заказ"
+            webView.visibility=View.VISIBLE
+        }
+
 
 
     }
